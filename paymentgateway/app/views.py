5717c280083f9b1935b0e1b6457b05487d5238dc
swapnacv21 <swapnavenu116@gmail.com>
2025-01-17 22:31:17 +0530

    payment gateway

--- paymentgateway/app/views.py
+++ paymentgateway/app/views.py
@@ -40,6 +40,31 @@ def call_back(request):
     def verify_signature(response_data):
         client = razorpay.Client(auth=(settings.RAZORPAY_KEY_ID,settings.RAZORPAY_KEY_SECRET))
         return client.utility.verify_payment_signature(response_data)
-    
+    if "razorpay_signature" in request.POST:
+        payment_id = request.POST.get("razorpay_payment_id","")
+        provider_order_id = request.POST.get("razorpay_order_id","")
+        signature_id = request.POST.get("razorpay_signature","")
+        order = Order.objects.get(provider_order_id=provider_order_id)
+        order.payment_id = signature_id
+        order.save()
+        if not verify_signature(request.POST):
+            order.status =  PaymentStatus.SUCCESS
+            order.save()
+            return render(request,"callback.html",context={"status":order.status})
+        else:
+            order.status = PaymentStatus.FAILURE
+            order.save()
+            return render(request,"callback.html",context={"status":order.status})
+    else:
+        payment_id = json.loads(request.POST.get("error[metadata]")).get("payment_id")
+        provider_order_id = json.loads(request.POST.get("error[metadata]")).get("order_id")
+        order = Order.objects.get(provider_order_id=provider_order_id)
+        order.payment_id =payment_id
+        order.status = PaymentStatus.FAILURE
+        order.save()
+        return render(request,"callback.html",context={"status":order.status})
 
+
+
+        
 
